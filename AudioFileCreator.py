993fa67ebde8b01f8c63bbf13e8c2d0eace0e1be
Manoel Almeida <manoel.almeida@bandtec.com.br>
2019-11-12 14:04:26 -0200

    Login no Azure funcionando

--- AudioFileCreator.py
+++ AudioFileCreator.py
@@ -4,14 +4,13 @@ conn = pyodbc.connect(
 	'Driver={ODBC Driver 17 for SQL Server};'
 	'Server=srvblindmarket.database.windows.net;'
 	'PORT=1433;'
-	'Database=dbblindmarket;'
-	'TrustServerCertificate=no;'
+	'Database=bdblindmarket;'
 	'UID=userblindmarket;'
 	'PWD=#Gfgrupo9;'
 	'Authentication=SqlPassword;')
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM bdblindmarket.Produto')
+cursor.execute('SELECT * FROM Produto')
 
 for row in cursor:
     print(row)
